1c4d3fb918bd0bb2cc99ab9efaead6f8520fd822
db0company <db0company@gmail.com>
2015-01-24 12:38:04 +0100

    api in URL /api/ (nothing in / for now)

--- schoolidolapi/urls.py
+++ schoolidolapi/urls.py
@@ -7,14 +7,14 @@ import api.views as api_views
 admin.autodiscover()
 
 router = routers.DefaultRouter()
-router.register(r'api/users', api_views.UserViewSet)
-router.register(r'api/cards', api_views.CardViewSet)
-router.register(r'api/events', api_views.EventViewSet)
+router.register(r'users', api_views.UserViewSet)
+router.register(r'cards', api_views.CardViewSet)
+router.register(r'events', api_views.EventViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    url(r'^', include(router.urls)),
+    url(r'^api/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^admin/', include(admin.site.urls)),
 ]

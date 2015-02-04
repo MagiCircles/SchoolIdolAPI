61a6d057302767aa49633d6d010f7da583035533
db0company <db0company@gmail.com>
2015-02-04 18:26:08 +0000

    web: Call objects methods directly from the templates yay

--- web/templatetags/getattribute.py
+++ web/templatetags/getattribute.py
@@ -9,6 +9,8 @@ def getattribute(value, arg):
     """Gets an attribute of an object dynamically from a string name"""
 
     if hasattr(value, str(arg)):
+        if callable(getattr(value, arg)):
+            return getattr(value, arg)()
         return getattr(value, arg)
     elif hasattr(value, 'has_key') and value.has_key(arg):
         return value[arg]

ea7a4e5e921477e3ef6159cc8b12bb75211089c8
db0company <db0company@gmail.com>
2015-02-05 08:21:54 -0800

    Show a default blank picture when the card is not available + fallback to url

--- web/templatetags/imageurl.py
+++ web/templatetags/imageurl.py
@@ -1,7 +1,15 @@
 from django import template
 register = template.Library()
 
-def imageurl(image):
-    return str(image).replace('web', '')
+def imageurl(card, image):
+    if hasattr(card, image):
+        card_image = getattr(card, image)
+        if card_image:
+            return str(card_image).replace('web', '')
+        elif hasattr(card, image.replace('image', 'url')):
+            url = getattr(card, image.replace('image', 'url'))
+            if url:
+                return url
+    return '/static/default-' + card.attribute + '.png'
 
 register.filter('imageurl', imageurl)

50b5d60fadff4bfff93d36d8bfb578239b219dcc
db0company <db0company@gmail.com>
2015-03-22 18:22:30 -0700

    Redirect /api/ to API documentation

--- schoolidolapi/urls.py
+++ schoolidolapi/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from rest_framework import routers
 import api.views as api_views
-#import web.views as web_views
+from django.views.generic.base import RedirectView
 
 admin.autodiscover()
 
@@ -17,6 +17,7 @@ router.register(r'events', api_views.EventViewSet)
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     url(r'^', include('web.urls')),
+    url(r'^api[/]+$', RedirectView.as_view(url='https://github.com/SchoolIdolTomodachi/SchoolIdolAPI/wiki/LoveLive!-School-Idol-API', permanent=False), name='api'),
     url(r'^api/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^admin/', include(admin.site.urls)),

95cc614e766cc067569f9c0ce20e3357ee950424
db0company <db0company@gmail.com>
2015-08-06 14:09:03 -0700

    Fix failindent js

--- web/static/js/loadactivities.js
+++ web/static/js/loadactivities.js
@@ -17,6 +17,9 @@ function loadMoreActivitiesOnClick(button, parent, account, feed, avatar_size) {
 	      loadMoreActivities(parent, account, feed, avatar_size);
 	      avatarStatus();
 	      updateActivities();
+	      // Reload disqus comments count
+	      window.DISQUSWIDGETS = undefined;
+	      $.getScript("http://schoolidol.disqus.com/count.js");
 	  });
 }
 

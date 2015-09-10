83c8da7e75df68f591c063c974336d5cc000016f
db0company <db0company@gmail.com>
2015-09-09 19:47:25 -0700

    Show skills icons on each cards

--- web/templatetags/choicesToString.py
+++ web/templatetags/choicesToString.py
@@ -9,6 +9,27 @@ def verifiedToString(value):
 def playWithToString(value):
     return models.playWithToString(value)
 
+skillsIcons = {
+    'Score Up': 'scoreup',
+    'Healer': 'healer',
+    'Perfect Lock': 'perfectlock',
+    'Perfect Charm': 'scoreup',
+    'Rhythmical Charm': 'scoreup',
+    'Timer Yell': 'healer',
+    'Timer Charm': 'scoreup',
+    'Rhythmical Yell': 'healer',
+    'Total Charm': 'scoreup',
+    'Total Trick': 'perfectlock',
+    'Perfect Yell': 'healer',
+    'Total Yell': 'healer',
+    'Timer Trick': 'perfectlock',
+}
+
+def skillToFlaticon(skill):
+    if skill in skillsIcons:
+        return skillsIcons[skill]
+    return 'skill'
+
 register.filter('verifiedToString', verifiedToString)
 register.filter('playWithToString', playWithToString)
 register.filter('activityMessageToString', models.activityMessageToString)
@@ -16,3 +37,4 @@ register.filter('userStatusToString', models.statusToString)
 register.filter('userStatusToColor', models.statusToColor)
 register.filter('userStatusToColorString', models.statusToColorString)
 register.filter('linkTypeToString', models.linkTypeToString)
+register.filter('skillToFlaticon', skillToFlaticon)

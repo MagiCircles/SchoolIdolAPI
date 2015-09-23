dc4981c75aaa79e9c2e7583edbaca95f82a75fbc
db0company <db0company@gmail.com>
2015-09-23 14:17:49 -0700

    Show verifications guide by default

--- web/static/js/editaccount.js
+++ web/static/js/editaccount.js
@@ -1,6 +1,13 @@
 
-$('#id_verification').change(function(e) {
+function showverificationdetails() {
     $('#verification-note').remove();
     $('#id_verification').closest('.form-group').after('<div id="verification-note">' + $('#verification-' + $('#id_verification').val()).html() + '</div>');
+}
+
+$('#id_verification').change(function(e) {
+    showverificationdetails();
 });
 
+$(document).ready(function() {
+    showverificationdetails();
+});

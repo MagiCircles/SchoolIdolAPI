cfaaf7e214dfaf951bfdb8a67a898b0b9a249d13
Enguerrand Decorne <decorne.en@gmail.com>
2015-10-13 14:57:19 +0200

    Various fixes

--- contest/templatetags/formattime.py
+++ contest/templatetags/formattime.py
@@ -2,10 +2,14 @@ from django import template
 register = template.Library()
 
 def formattime(date, fmt):
-    return date.strftime(fmt) 
+    return date.strftime(fmt)
 
 def shortname(name):
     return name.split(' ')[-1]
 
+def formatseconds(seconds):
+    return int(seconds / 3600)
+
 register.filter('formattime', formattime)
+register.filter('formatseconds', formatseconds)
 register.filter('shortname', shortname)

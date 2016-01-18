c9b5ba3a09a7148c268ab8ba9ba3e4f1a8279a28
Enguerrand Decorne <decorne.en@gmail.com>
2016-01-18 15:56:37 +0000

    Add importvotes

---
+++ api/management/commands/importvotes.py
@@ -0,0 +1,24 @@
+from django.core.management.base import BaseCommand, CommandError
+import api.models as am
+import contest.models as ac
+import sys
+import json
+from urlparse import parse_qs
+import django.utils.dateparse as dateparse
+
+class Command(BaseCommand):
+    can_import_settings = True
+
+    def handle(self, *args, **options):
+        file_handle = open('votes.json', 'r')
+        votes = json.loads(file_handle.read())
+        file_handle.close()
+
+        for vote in votes:
+            print vote
+            new_vote = ac.Vote()
+            new_vote.idolized = vote['idolized']
+            new_vote.counter = vote['counter']
+            new_vote.contest = ac.Contest.objects.get(pk=vote['contest'])
+            new_vote.card = am.Card.objects.get(id=vote['card'])
+            new_vote.save()

c3b53a181510dda5f15f63eeb869827acbc47429
db0company <db0company@gmail.com>
2016-03-24 10:05:18 -0400

    List of Rs to use to level up the skill of promos

--- api/management/commands/update_skill_up_cards.py
+++ api/management/commands/update_skill_up_cards.py
@@ -1,24 +1,22 @@
 from django.core.management.base import BaseCommand, CommandError
+from web.templatetags.choicesToString import skillsIcons
 from api import models
 
 def update_skill_up_cards(opt={}):
     print '## Update skill up cards'
-    promo_skills = [s['skill'] for s in models.Card.objects.filter(is_promo=True).order_by('skill').values('skill').distinct()]
+    all_skills = [skill for skill in skillsIcons.keys() if skill != 'Score Up' and skill != 'Healer' and skill != 'Perfect Lock']
     skills = {}
-    for skill in promo_skills:
-        skills[skill] = ','.join([str(card['id']) + '-' + card['name'].split(' ')[-1] for card in models.Card.objects.filter(is_promo=False, skill=skill).values('id', 'name')])
+    for skill in all_skills:
+        skills[skill] = ','.join([str(card['id']) + '-' + card['name'].split(' ')[-1] for card in models.Card.objects.filter(is_promo=False, skill=skill).exclude(idol__main_unit='A-RISE').exclude(idol__main_unit='Aqours').values('id', 'name')])
     print skills
-    promo_cards = models.Card.objects.filter(is_promo=True)
+    promo_cards = models.Card.objects.filter(skill__in=all_skills)
     for card in promo_cards:
         previous_skill_up_cards = card._skill_up_cards
-        card._skill_up_cards = skills[card.skill]
-        if previous_skill_up_cards != card._skill_up_cards:
-            card.save()
-            print '  #{} {}'.format(card, card._skill_up_cards)
-    promo_cards = models.Card.objects.filter(is_promo=True)
-    for card in promo_cards:
-        previous_skill_up_cards = card._skill_up_cards
-        card._skill_up_cards = skills[card.skill]
+        card._skill_up_cards = skills[card.skill].replace(str(card.id) + '-' + card.name.split(' ')[-1], '').replace(',,',',')
+        if card._skill_up_cards[-1] == ',':
+            card._skill_up_cards = card._skill_up_cards[:-1]
+        if card._skill_up_cards[0] == ',':
+            card._skill_up_cards = card._skill_up_cards[1:]
         if previous_skill_up_cards != card._skill_up_cards:
             card.save()
             print '  #{} {}'.format(card, card._skill_up_cards)

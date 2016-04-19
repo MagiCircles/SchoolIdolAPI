e028606c58cf5f98461bd7d2aaf49cabb45347e2
db0company <db0company@gmail.com>
2016-04-19 11:07:39 +0100

    Fix users permissions

--- api/permissions.py
+++ api/permissions.py
@@ -26,7 +26,12 @@ class IsStaffOrReadOnly(permissions.BasePermission):
 class UserPermissions(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        return request.method == 'GET' or request.method == 'POST' or request.user.is_superuser
+        return True
 
     def has_object_permission(self, request, view, obj=None):
-        return request.method == 'POST' or request.user.is_superuser or request.user == obj
+        return (
+            request.method in permissions.SAFE_METHODS
+            or obj is None
+            or request.user.is_staff
+            or obj == request.user
+        )

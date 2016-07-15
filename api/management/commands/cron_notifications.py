21ac167e827cde53ccbb60f713c8190ec2ff6bda
db0company <db0company@gmail.com>
2016-07-15 08:11:31 -0700

    Handle errors in cron notifications

--- api/management/commands/cron_notifications.py
+++ api/management/commands/cron_notifications.py
@@ -12,17 +12,21 @@ class Command(BaseCommand):
         for notification in notifications:
             preferences = notification.owner.preferences
             if preferences.is_notification_email_allowed(notification.message):
-                send_email(
-                    subject=(u'School Idol Tomodachi' + u'✨ ' + u' Notification: ' + notification.english_message),
-                    template_name = 'notification',
-                    to=[notification.owner.email, 'contact@schoolido.lu'],
-                    context={
-                        'notification': notification,
-                        'user': notification.owner,
-                    },
-                )
+                try:
+                    send_email(
+                        subject=(u'School Idol Tomodachi' + u'✨ ' + u' Notification: ' + notification.english_message),
+                        template_name = 'notification',
+                        to=[notification.owner.email, 'contact@schoolido.lu'],
+                        context={
+                            'notification': notification,
+                            'user': notification.owner,
+                        },
+                    )
+                    print 'Email sent to {}: {}'.format(notification.owner.username, notification.localized_message)
+                except Exception, e:
+                    print '!! Error when sending email to {} !!'.format(notification.owner.email)
+                    print e
                 # todo ios push notifications
-                print 'Email sent to {}: {}'.format(notification.owner.username, notification.localized_message)
             else:
                 print '  No email for {}: {}'.format(notification.owner.username, notification.localized_message)
             notification.email_sent = True

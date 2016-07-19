7183530f63f3538099abe7c26f963bee40fd82e8
db0company <db0company@gmail.com>
2016-07-19 07:24:06 -0700

    Do not exclude Aqours in skill up cards since they're not sticker shop only anymore

--- api/management/commands/update_skill_up_cards.py
+++ api/management/commands/update_skill_up_cards.py
@@ -7,7 +7,7 @@ def update_skill_up_cards(opt={}):
     all_skills = [skill for skill in skillsIcons.keys() if skill != 'Score Up' and skill != 'Healer' and skill != 'Perfect Lock']
     skills = {}
     for skill in all_skills:
-        skills[skill] = ','.join([str(card['id']) + '-' + card['name'].split(' ')[-1] for card in models.Card.objects.filter(is_promo=False, skill=skill).exclude(idol__main_unit='A-RISE').exclude(idol__main_unit='Aqours').values('id', 'name')])
+        skills[skill] = ','.join([str(card['id']) + '-' + card['name'].split(' ')[-1] for card in models.Card.objects.filter(is_promo=False, skill=skill).exclude(idol__main_unit='A-RISE').values('id', 'name')])
     print skills
     promo_cards = models.Card.objects.filter(skill__in=all_skills)
     for card in promo_cards:

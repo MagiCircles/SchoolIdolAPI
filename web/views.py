e9fd1ccb16e31ce23d7165f5fead960a55ed4786
db0company <db0company@gmail.com>
2016-07-28 22:14:41 -0700

    Fix english_future

--- web/views.py
+++ web/views.py
@@ -2947,9 +2947,9 @@ def trades_or_giveaways_submit(request, type, account):
 
 def english_future(request):
     context = globalContext(request)
-    future_events = models.Event.objects.filter(Q(english_beginning=None) | Q(english_end__gte=timezone.now())).order_by('beginning')
+    future_events = models.Event.objects.filter(Q(english_beginning=None) | Q(english_end__gte=timezone.now())).order_by('-english_beginning', 'beginning')
     # remove events already paired
-    future_events = future_events.exclude(pk__in=[53])
+    future_events = future_events.exclude(pk__in=[53,58])
     # remove too old events (not gonna happen)
     future_events = future_events.exclude(beginning__lte=timezone.now() - relativedelta(months=15))
     # add cards

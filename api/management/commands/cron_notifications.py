d12dce62314e56a735bf472f012e036d2acbba38
db0company <db0company@gmail.com>
2016-08-17 13:03:32 -0700

    Avoid duplicate notifications

--- api/management/commands/cron_notifications.py
+++ api/management/commands/cron_notifications.py
@@ -8,27 +8,32 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
+        sent = []
         notifications = models.Notification.objects.filter(email_sent=False).select_related('owner', 'owner__preferences')
         for notification in notifications:
             preferences = notification.owner.preferences
             if preferences.is_notification_email_allowed(notification.message):
-                try:
-                    send_email(
-                        subject=(u'School Idol Tomodachi' + u'✨ ' + u' Notification: ' + notification.english_message),
-                        template_name = 'notification',
-                        to=[notification.owner.email],
-                        context={
-                            'notification': notification,
-                            'user': notification.owner,
-                        },
-                    )
-                    print 'Email sent to {}: {}'.format(notification.owner.username, notification.localized_message)
-                except Exception, e:
-                    print '!! Error when sending email to {} !!'.format(notification.owner.email)
-                    print e
-                    notification.owner.preferences.invalid_email = True
-                    notification.owner.preferences.save()
-                # todo ios push notifications
+                notification_sent = notification.owner.email + notification.english_message + notification.website_url
+                if notification_sent in sent:
+                    print u' Duplicate not sent to {}: {}'.format(notification.owner.username, notification.english_message)
+                else:
+                    try:
+                        send_email(
+                            subject=(u'School Idol Tomodachi' + u'✨ ' + u' Notification: ' + notification.english_message),
+                            template_name = 'notification',
+                            to=[notification.owner.email],
+                            context={
+                                'notification': notification,
+                                'user': notification.owner,
+                            },
+                        )
+                        sent.append(notification_sent)
+                        print u'Email sent to {}: {}'.format(notification.owner.username, notification.english_message)
+                    except Exception, e:
+                        print u'!! Error when sending email to {} !!'.format(notification.owner.email)
+                        print e
+                        notification.owner.preferences.invalid_email = True
+                        notification.owner.preferences.save()
             else:
                 print '  No email for {}: {}'.format(notification.owner.username, notification.localized_message)
             notification.email_sent = True

f4c79230afd56e089772983ad336f289f3122a74
db0company <db0company@gmail.com>
2017-12-20 21:19:31 +0100

    Use BaseAccount for sukutomo account model

--- magi/sukutomo/forms.py
+++ magi/sukutomo/forms.py
@@ -1,10 +1,17 @@
+import datetime
 from magi import forms
+from django.core.validators import MinValueValidator
+from django.utils.translation import ugettext_lazy as _
+from magi.utils import PastOnlyValidator
 
 class AccountForm(forms.AccountForm):
+    start_date = forms.forms.DateField(required=False, label=_('Start Date'), validators=[
+        PastOnlyValidator,
+        MinValueValidator(datetime.date(2013, 4, 16)),
+    ])
+
     def __init__(self, *args, **kwargs):
         super(AccountForm, self).__init__(*args, **kwargs)
-        #if 'starter' in self.fields:
-        #    self.fields['starter'].queryset = models.Card.objects.filter(pk__in=STARTERS)
         if 'fake' in self.fields:
             del(self.fields['fake'])
         if 'transfer_code' in self.fields:
@@ -13,6 +20,6 @@ class AccountForm(forms.AccountForm):
             self.fields['nickname'].initial = self.request.user.username
 
     class Meta(forms.AccountForm.Meta):
-        optional_fields = ('rank', 'friend_id', 'accept_friend_requests', 'device', 'start_date',
+        optional_fields = ('level', 'friend_id', 'accept_friend_requests', 'device', 'start_date',
                            'loveca', 'friend_points', 'g', 'tickets', 'vouchers', 'bought_loveca',
                            'i_play_with', 'i_os')
--- magi/sukutomo/models.py
+++ magi/sukutomo/models.py
@@ -4,24 +4,23 @@ from django.core.validators import MinValueValidator
 from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _, string_concat
 from django.db import models
+from django.utils import timezone
 from magi.utils import PastOnlyValidator
-from magi.item_model import MagiModel, CacheOwner, i_choices
+from magi.abstract_models import BaseAccount
+from magi.item_model import MagiModel, i_choices
 from sukutomo.django_translated import t
 
-class Account(CacheOwner):
+class Account(BaseAccount):
     collection_name = 'account'
 
     # Foreign keys
 
-    owner = models.ForeignKey(User, related_name='accounts')
     #center = models.ForeignKey('OwnedCard', verbose_name=_('Center'), null=True, help_text=_('The character that talks to you on your home screen.'), on_delete=models.SET_NULL)
     #starter = models.ForeignKey(Card, verbose_name=_('Starter'), null=True, help_text=_('The character that you selected when you started playing.'), on_delete=models.SET_NULL)
 
     # Details
 
-    creation = models.DateTimeField(auto_now_add=True) # new
     nickname = models.CharField(_('Nickname'), max_length=20)
-    rank = models.PositiveIntegerField(_('Rank'), null=True)
 
     VERSIONS = OrderedDict((
         ('JP', { 'translation': t['Japanese'], 'icon': 'JP' }),
@@ -38,19 +37,12 @@ class Account(CacheOwner):
     show_friend_id = models.BooleanField(_('Should your friend ID be visible to other players?'), default=True)
     accept_friend_requests = models.NullBooleanField(_('Accept friend requests'), null=True)
     device = models.CharField(_('Device'), help_text=_('The model of your device. Example: Nexus 5, iPhone 4, iPad 2, ...'), max_length=150, null=True)
-    # previously: creation
-    start_date = models.DateField(blank=True, null=True, verbose_name=_('Start Date'), validators=[
-        MinValueValidator(datetime.date(2013, 4, 16)),
-        PastOnlyValidator,
-    ])
-    # previously: show_creation
-    show_start_date = models.BooleanField('', default=True, help_text=_('Should this date be visible to other players?'))
     loveca = models.PositiveIntegerField(_('Love gems'), help_text=string_concat(_('Number of {} you currently have in your account.').format(_('Love gems')), ' ', _('This field is completely optional, it\'s here to help you manage your accounts.')), default=0)
     friend_points = models.PositiveIntegerField(_('Friend Points'), help_text=string_concat(_('Number of {} you currently have in your account.').format(_('Friend Points')), ' ', _('This field is completely optional, it\'s here to help you manage your accounts.')), default=0)
     g = models.PositiveIntegerField('G', help_text=string_concat(_('Number of {} you currently have in your account.').format('G'), ' ', _('This field is completely optional, it\'s here to help you manage your accounts.')), default=0)
     tickets = models.PositiveIntegerField('Scouting Tickets', help_text=string_concat(_('Number of {} you currently have in your account.').format('Scouting Tickets'), ' ', _('This field is completely optional, it\'s here to help you manage your accounts.')), default=0)
     vouchers = models.PositiveIntegerField('Vouchers (blue tickets)', help_text=string_concat(_('Number of {} you currently have in your account.').format('Vouchers (blue tickets)'), ' ', _('This field is completely optional, it\'s here to help you manage your accounts.')), default=0)
-    bought_loveca = models.PositiveIntegerField(_('Total love gems bought'), help_text=_('You can calculate that number in "Other" then "Purchase History". Leave it empty to stay F2P.'), null=True)
+    bought_loveca = models.PositiveIntegerField(_('Total love gems bought'), help_text=_('You can calculate that number in "Other" then "Purchase History".'), null=True)
     show_items = models.BooleanField('', default=True, help_text=_('Should your items be visible to other players?'))
 
     # Choices
@@ -95,39 +87,11 @@ class Account(CacheOwner):
     #verified = models.PositiveIntegerField(_('Verified'), default=0, choices=VERIFIED_CHOICES)
     #default_tab = models.CharField(_('Default tab'), max_length=30, choices=ACCOUNT_TAB_CHOICES, help_text=_('What people see first when they take a look at your account.'), default='deck')
 
-    # Cache: leaderboard position
-
-    _cache_leaderboards_days = 1
-    _cache_leaderboards_last_update = models.DateTimeField(null=True)
-    _cache_leaderboard = models.PositiveIntegerField(null=True)
-    _cache_leaderboard_version = models.PositiveIntegerField(null=True)
+    # Cache: leaderboard per version
 
     def update_cache_leaderboards(self):
         self._cache_leaderboards_last_update = timezone.now()
-        self._cache_leaderboard = getAccountLeaderboard(self)
-        self._cache_leaderboard_version = getAccountLeaderboardForVersion(self)
-
-    def force_cache_leaderboards(self):
-        self.update_cache_leaderboards()
-        self.save()
-
-    @property
-    def cached_leaderboard(self):
-        if not self._cache_leaderboards_last_update or self._cache_leaderboards_last_update < timezone.now() - datetime.timedelta(days=self._cache_leaderboards_days):
-            self.force_cache_leaderboards()
-        return self._cache_leaderboard
-
-    @property
-    def cached_leaderboard_team(self):
-        if not self._cache_leaderboards_last_update or self._cache_leaderboards_last_update < timezone.now() - datetime.timedelta(days=self._cache_leaderboards_days):
-            self.force_cache_leaderboards()
-        return self._cache_leaderboard_team
-
-    def __unicode__(self):
-        return u'{} {} '.format(self.nickname, self.version)
-
-def getAccountLeaderboard(account):
-    return Account.objects.filter(rank__gt=account.rank).values('rank').distinct().count() + 1
-
-def getAccountLeaderboardForVersion(account):
-    return Account.objects.filter(i_version=account.i_version, rank__gt=account.rank).values('rank').distinct().count() + 1
+        self._cache_leaderboard = type(self).objects.filter(
+            level__gt=self.level,
+            i_version=self.i_version,
+        ).values('level').distinct().count() + 1

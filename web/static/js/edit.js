606243a0219872871d16e8555e22e21c0cf8e5dd
db0company <db0company@gmail.com>
2018-02-18 10:12:24 +0800

    Add trailing slash at the end of request to delete link to avoid double redirection

--- web/static/js/edit.js
+++ web/static/js/edit.js
@@ -2,7 +2,7 @@
 $('a[href="#deleteLink"]').click(function(e) {
     e.preventDefault();
     var link = $(this).closest('tr');
-    $.get('/ajax/deletelink/' + $(this).attr('data-link-id'), function(data) {
+    $.get('/ajax/deletelink/' + $(this).attr('data-link-id') + '/', function(data) {
 	if (data == 'deleted') {
 	    link.remove();
 	}

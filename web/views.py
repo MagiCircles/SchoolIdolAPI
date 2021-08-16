69e3f07735361c50e7a71a0e1f1427762c83c2c4
Victor Hirumitsu <vmhirumitsu@gmail.com>
2021-08-16 02:05:16 -0300

    Change card japan_only field default value to false and adjust events to work after the SIF servers merge

--- web/views.py
+++ web/views.py
@@ -273,7 +273,7 @@ def pushActivity(message, number=None, ownedcard=None, eventparticipation=None, 
 BANNERS_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRsbOHP9SBVItTAtbdyuAL7_F6zjPRCzbFGaQs_gEWHYTCajR7N1bmOZ--caMZIQ4yYUs2_-cIfp7Ik/pub?gid=0&single=true&output=csv'
 _news = None
 _last_news_update = None
-    
+
 def index(request):
     """
     SQL Queries:
@@ -2115,6 +2115,7 @@ def event(request, event):
     context['soon_happen_japan'] = event.soon_happen_japan()
     context['is_world_current'] = event.is_world_current()
     context['is_japan_current'] = event.is_japan_current()
+    context['is_legacy'] = event.legacy
 
     # get cards
     event.all_cards = event.cards.all()
@@ -2125,10 +2126,13 @@ def event(request, event):
 
     # get rankings
     if context['did_happen_japan']:
-        event.japanese_participations = event.participations.filter(account_language='JP').extra(select={'ranking_is_null': 'ranking IS NULL'}, order_by=['ranking_is_null', 'ranking'])[:10]
-        if context['did_happen_world']:
-            event.english_participations = event.participations.filter(account_language='EN').extra(select={'ranking_is_null': 'ranking IS NULL'}, order_by=['ranking_is_null', 'ranking'])[:10]
-            event.other_participations = event.participations.exclude(account_language='JP').exclude(account_language='EN').extra(select={'ranking_is_null': 'ranking IS NULL'}, order_by=['account_language', 'ranking_is_null', 'ranking'])
+        if event.legacy:
+            event.japanese_participations = event.participations.filter(account_language='JP').extra(select={'ranking_is_null': 'ranking IS NULL'}, order_by=['ranking_is_null', 'ranking'])[:10]
+            if context['did_happen_world']:
+                event.english_participations = event.participations.filter(account_language='EN').extra(select={'ranking_is_null': 'ranking IS NULL'}, order_by=['ranking_is_null', 'ranking'])[:10]
+                event.other_participations = event.participations.exclude(account_language='JP').exclude(account_language='EN').extra(select={'ranking_is_null': 'ranking IS NULL'}, order_by=['account_language', 'ranking_is_null', 'ranking'])
+        else:
+            event.all_participations = event.participations.extra(select={'ranking_is_null': 'ranking IS NULL'}, order_by=['ranking_is_null', 'ranking', 'account_language'])[:10]
 
     context['event'] = event
     context['event_links'] = (link for link in links_list if link['link'] == 'events').next()

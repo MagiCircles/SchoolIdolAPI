f7083b8562c2c66e6021cdc294828ee978dbb83c
db0company <db0company@gmail.com>
2021-10-06 15:18:12 +0200

    Use tinify instead of tinypng

--- web/utils.py
+++ web/utils.py
@@ -4,15 +4,14 @@ import os
 import string
 import random
 import re
+import tinify
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import get_template
 from django.template import Context
 from django.core.files.temp import NamedTemporaryFile
-from tinypng import shrink_file
 from django.core.files.images import ImageFile
 from django.core.files.uploadedfile import InMemoryUploadedFile
 from django.utils.http import urlquote
-from tinypng.api import shrink_data
 from django.conf import settings
 from api.raw import all_chibis
 
@@ -41,21 +40,9 @@ def shrinkImageFromData(data, filename='lol.png'):
     api_key = settings.TINYPNG_API_KEY
     if not api_key or extension not in ['.png', '.jpg', '.jpeg']:
         return dataToImageFile(data)
-    info, new_data = shrink_data(data, api_key)
-    return dataToImageFile(new_data)
-
-def shrunkImage(picture, filename):
-    api_key = settings.TINYPNG_API_KEY
-    if not api_key or not filename.endswith('.png'):
-        return picture
-    img_shrunked = NamedTemporaryFile(delete=False)
-    shrink_info = shrink_file(
-            picture.name,
-            api_key=api_key,
-            out_filepath=img_shrunked.name
-    )
-    img_shrunked.flush()
-    return ImageFile(img_shrunked)
+    tinify.key = api_key
+    source = tinify.from_buffer(data)
+    return dataToImageFile(source.to_buffer())
 
 def send_email(subject, template_name, to=[], context={}, from_email=settings.AWS_SES_RETURN_PATH):
     if 'template_name' != 'notification':

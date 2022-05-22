d52f73ab17f544bb009c8ac8b25c517e46debad7
Victor Hirumitsu <vmhirumitsu@gmail.com>
2022-05-22 20:24:04 -0300

    Add main unit list to generated settings

--- web/views.py
+++ web/views.py
@@ -730,7 +730,7 @@ def get_cards_form_filters(request, cardsinfo):
         'collections': cardsinfo['collections'],
         'translated_collections': cardsinfo['translated_collections'],
         'sub_units': cardsinfo['sub_units'] if 'sub_units' in cardsinfo else [],
-        'main_units': ['Aqours', 'μ\'s'],
+        'main_units': cardsinfo['main_units'],
         'skills': cardsinfo['skills'],
         'rarity_choices': models.RARITY_CHOICES,
         'attribute_choices': models.ATTRIBUTE_CHOICES,
